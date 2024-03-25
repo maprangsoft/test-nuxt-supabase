544deb69b6278eff05d92c3a83529a7f7d705240
maprangsoft <maprangsoft@gmail.com>
2024-03-25 21:41:29 +0700

    1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  ssr: false,
+  app: {
+    head: {
+        script: [
+            {
+              type: 'module',
+                src: 'https://cdn.jsdelivr.net/gh/Young-Einstein10/messaging-widget@main/main.js',
+            }
+        ],
+    }
+}
 })
